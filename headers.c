144d5cb5ac32d472b249c93557fc1aba1e621e11
Martin Estevez <martin.estevez@udla.edu.ec>
2026-05-08 15:18:48 -0500

    Creacion de restar

--- headers.c
+++ headers.c
@@ -1,17 +1,18 @@
-#include "headers.h"
+#include "restar.h"
+#include "sumar.h"
+#include <stdio.h>
 
-int main()
-{
-    int a, b;
-    printf("Ingrese el primer numero: ");
-    scanf("%d", &a);
-    printf("Ingrese el segundo numero: ");
-    scanf("%d", &b);
-    printf("La suma es: %d\n", sumar(a, b));
-    return 0;
-}
+int main() {
+  int a, b;
+  printf("Ingrese el primer numero");
+  printf("Ingrese el primer numero: ");
+  scanf("%d", &a);
+  printf("Ingrese el segundo numero");
+  printf("Ingrese el segundo numero: ");
+  scanf("%d", &b);
 
-int sumar(int a, int b)
-{
-    return a + b;
+  printf("La suma es %d", sumar(a, b));
+  printf("La suma es %d\n", sumar(a, b));
+  printf("La resta es %d\n", restar(a, b));
+  return 0;
 }
